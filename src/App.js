5f84947ba7d86c340dff9a30972bf92f5d08bc8a
Choiji92 <jeahoon100@naver.com>
2022-06-25 17:27:09 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Main from "./pages/Main";
+import GlobalStyles from "./assests/css/GlobalStyles";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Mypage from "./pages/Mypage";
+import Posts from "./pages/Posts";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+      <Header/>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/write/:id" element={<Write />} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Routes>
     </div>
   );
 }
