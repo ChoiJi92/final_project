da7bd4722236e419f5ad9c51e90607376ea47921
Choiji92 <jeahoon100@naver.com>
2022-07-03 13:19:56 +0900

    test #17

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -2,9 +2,11 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
 
 const UserMenu = ({userImage,nickName}) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const navigate = useNavigate()
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -34,9 +36,12 @@ const UserMenu = ({userImage,nickName}) => {
           'aria-labelledby': 'basic-button',
         }}
       >
+        <MenuItem onClick={()=>{
+          navigate('/userwrite')
+          handleClose()
+        } }>글쓰기</MenuItem>
         <MenuItem onClick={handleClose}>숙소 호스트 되기</MenuItem>
         <MenuItem onClick={handleClose}>계정</MenuItem>
-        {/* <MenuItem onClick={handleClose}>Logout</MenuItem> */}
         <MenuItem onClick={()=>{
             localStorage.clear()
             handleClose()
