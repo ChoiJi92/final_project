ef128905c49fb55b0e7828533db65c226b217ac8
natural-nine <wan.jun.bruce@gmail.com>
2022-07-08 14:18:48 +0900

    이미지 슬라이드, 이미지 다이얼로그 수정 중 #53

--- src/pages/HouseInfo.js
+++ src/pages/HouseInfo.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
-import {FaHeart} from "react-icons/fa"
+import {FaHeart,FaStar} from "react-icons/fa"
 import styled from "styled-components";
 import room2 from "../assests/css/room2.jpeg"
 import room1 from "../assests/css/room1.jpeg"
 import jeju1 from "../assests/css/jeju1.jpeg"
 import jeju2 from "../assests/css/jeju2.jpeg"
-import { useNavigate } from "react-router-dom";
+import jeju3 from "../assests/css/jeju3.jpeg"
+import jeju4 from "../assests/css/jeju4.jpeg"
+import jeju5 from "../assests/css/jeju5.jpeg"
+import jeju6 from "../assests/css/jeju6.jpeg"
+import { Link, useNavigate } from "react-router-dom";
 import SlideImg from "../components/SlideImg";
+
 
 const HouseInfo = () => {
     const [isList, setIsList] = useState([]);
 
     const navigate = useNavigate();
     const list = [1,2,3,4,5,6,7,8,9]
-    const listImg = [room2, room1, jeju1, jeju2];
+    const listImg = [room2, room1, jeju1, jeju2, jeju3, jeju4,jeju5,jeju6];
     useEffect(()=>{
     
     setIsList(list)
@@ -27,103 +32,126 @@ const HouseInfo = () => {
     return(
         <MainBox>
             <ContentsBox>
-                <ContentsList>
                     {isList.map((item, idx)=>{
                         return(
                         <ContentsListBox key={idx}>
                             <SlideImg listImg={listImg}/>
-                          
                             <DesBox>
-                                <h5>해변가에서의 한달살기</h5>
-                                <span>한달살기의 조건에 관한 설명 ...</span>
-                                <span>한달살기의 조건에 관한 설명 ...</span>
+                                <StyledLink style={{ textDecoration: 'none' }} to={`/house/${idx}`}>
+                                <h3>해변근처의 게스트하우스ㅁㅁㅁ</h3>
+                                </StyledLink>
+                                <div id="infoHouse">
+                                <span>한달살기의 조건에 관한 설명 ...한달살기의 조건에 관한 설명 ...
+                                한달살기의 조건에 관한 설명 ...
+                                </span>
+                                
+                                </div>
+                                <span>000,000원 1박</span>
+                                <LikeBox>
+                                    
+                                    <StarIcon/>
+                                    
+                                    
+                                    <HeartIcon onClick={()=>{onClick(isList[idx])}}/>
+                                    
+                                </LikeBox>
                             </DesBox>
-                            <LikeBox>
-                                <Icon onClick={()=>{onClick(isList[idx])}}/>
-                            </LikeBox>
-
-                            {/* <img src={room1} style={{"width" : "100px", "height":"100px"}}/>
-                            <div style={{"display":"flex", "flexDirection" :"column","marginLeft":"10px"}}>
-                                <h5>해변가에서의 한달살기</h5>
-                                <span>한달살기의 조건에 관한 설명 ...</span>
-                                <span>한달살기의 조건에 관한 설명 ...</span>
-                            </div> */}
+                            
                         </ContentsListBox>
                         )  
                     })}
-                    
-                </ContentsList>
             </ContentsBox> 
-            <Map></Map>
+            <Map>
+                지도 들어갈 예정
+            </Map>
         </MainBox>
     );
 };
     
 
-const Wrap = styled.div`
 
-`
 
 const MainBox = styled.div`
     display: flex;
-    justify-content: space-between;
-    justify-content: center;
-    margin-top: 80px;
+    width: 100%;
+    margin-top: 30px;
 `
-
-const Map = styled.div`
-    width: 400px;
-    height: 350px;
-    border: 1px solid black;
-` 
 const ContentsBox = styled.div`
-    width: 300px;
-    height: 500px;
+    width: 40%;
+    height: 700px;
     overflow-y: auto;
-    margin-right: 10px;
+    border: 1px solid black;
+    margin-left: 200px;
+    display: flex;
+    flex-direction: column;
 `
-
-const ContentsList = styled.div`
-    
-   
-`
-
 const ContentsListBox = styled.div`
-    width: 530px;
     height: 300px;  
     display: flex;
     border-bottom: 0.5px solid gray;
-    align-items: center;
-    justify-content: space-between;
     
 `
-
 const DesBox = styled.div`
-    /* display: flex;
+    display: flex;
     flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    span{
-        font-size: 5px;
-    } */
-    
-    span{
-        display: flex;
-        margin-top: 2px;
-        font-size: 5px; 
+    margin-right: 10px;
+    width: 35%;
+    height: 270px;
+    margin-left: 20px;
+    margin-top: 15px;
+    h3{
+
     }
-    margin-bottom: 50px;
+    span{
+        margin-bottom: 30px;
+    }
+    #infoHouse{
+        height: 200px;
+        display: flex;
+        align-items: center;
+        margin-top: 25px;
+    }
 `
 const LikeBox = styled.div`
-    width: 80px;
-    height: 100px;  
+    display:flex;
+    justify-content: space-between;
+    height: 30px;
+    align-items: center;
+    margin-top: 10px;
 `
 
-const Icon = styled(FaHeart)`
-    font-size: 12px;
+const HeartIcon = styled(FaHeart)`
     color: gray;
-    margin-top: 90px;
-    margin-left: 30px;
     cursor: pointer;
+    margin-top: -5px;
 `
+const StarIcon = styled(FaStar)`
+    margin-top: -5px;
+`
+
+
+
+const Map = styled.div`
+    width: 50%;
+    height: 500px;
+    border: 1px solid black;
+    margin-right: 5px;
+    display: flex ;
+    justify-content: center;
+    align-items: center;
+` 
+
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: inherit;
+    &:focus, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+    :hover{
+        color: #3498db;
+    }
+`;
+
+
 export default HouseInfo;
